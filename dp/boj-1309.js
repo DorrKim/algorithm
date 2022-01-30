f1d30a59985338cfc42972a44dedab272853eca1
Kim hyunseok <dorrdorr9311@gmail.com>
2022-01-30 22:25:25 +0900

    boj-1309 동물원 해결

---
+++ dp/boj-1309.js
@@ -0,0 +1,18 @@
+const countCage = +require('fs').readFileSync('dev/stdin').toString().trim();
+
+function solution(countCage) {
+  const cache = new Array(countCage);
+  cache[0] = [2, 1];
+
+  for (let i = 1; i < countCage; i++) {
+    const [usePrev, notusePrev] = cache[(i - 1) % 2];
+    const useCurr = (usePrev + 2 * notusePrev) % 9901;
+    const notUseCurr = (usePrev + notusePrev) % 9901;
+    cache[i % 2] = [useCurr, notUseCurr];
+  }
+
+  const result = cache[(countCage - 1) % 2][0] + cache[(countCage - 1) % 2][1];
+  console.log(result % 9901);
+}
+
+solution(countCage);

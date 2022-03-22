cc09f7b0d926dc54ecae4f5261ff498b38fa9438
Kim hyunseok <dorrdorr9311@gmail.com>
2022-03-22 18:19:46 +0900

    boj-4256 트리 문제 해결

--- tree/boj-4256.js
+++ tree/boj-4256.js
@@ -16,30 +16,37 @@ function solution(TEST_CASES) {
 }
 
 function getPostOrder(testCase) {
-  const [_, preOrder, inOrder] = testCase;
+  const [[N], preOrder, inOrder] = testCase;
 
   let postOrderResult = '';
 
-  getTreeRoot(preOrder, inOrder);
+  getTreeRoot([preOrder, 0, N - 1], [inOrder, 0, N - 1]);
   return postOrderResult.trim();
 
-  function getTreeRoot(preOrder, inOrder) {
-    if (preOrder.length === 0 && inOrder.length === 0) return;
-    const rootValue = preOrder[0];
+  function getTreeRoot([preOrder, preOrderStart, preOrderEnd], [inOrder, inOrderStart, inOrderEnd]) {
+    if (preOrderStart > preOrderEnd || inOrderStart > inOrderEnd) return;
+    const rootValue = preOrder[preOrderStart];
 
-    const inOrderRootIndex = inOrder.findIndex((el) => el === rootValue);
-    const leftInOrder = inOrder.slice(0, inOrderRootIndex);
-    const rightInOrder = inOrder.slice(inOrderRootIndex + 1);
-
-    const leftPreOrder = preOrder.slice(1, leftInOrder.length + 1);
-    const rightPreOrder = preOrder.slice(leftInOrder.length + 1);
-
-    if (leftPreOrder.length && leftInOrder.length) {
-      getTreeRoot(leftPreOrder, leftInOrder);
+    let inOrderRootIndex;
+    for (let i = inOrderStart; i <= inOrderEnd; i++) {
+      if (inOrder[i] !== rootValue) continue;
+      inOrderRootIndex = i;
+      break;
     }
-    if (rightPreOrder.length && rightInOrder.length) {
-      getTreeRoot(rightPreOrder, rightInOrder);
-    }
+
+    const leftInOrderStart = inOrderStart;
+    const leftInOrderEnd = inOrderRootIndex - 1;
+    const rightInOrderStart = inOrderRootIndex + 1;
+    const rightInOrderEnd = inOrderEnd;
+
+    const leftPreOrderStart = preOrderStart + 1;
+    const leftPreOrderEnd = leftPreOrderStart + leftInOrderEnd - leftInOrderStart;
+    const rightPreOrderStart = leftPreOrderEnd + 1;
+    const rightPreOrderEnd = preOrderEnd;
+
+    getTreeRoot([preOrder, leftPreOrderStart, leftPreOrderEnd], [inOrder, leftInOrderStart, leftInOrderEnd]);
+    getTreeRoot([preOrder, rightPreOrderStart, rightPreOrderEnd], [inOrder, rightInOrderStart, rightInOrderEnd]);
+
     postOrderResult += `${rootValue} `;
   }
 }

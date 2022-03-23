4f31fe0c659eea6db9700fbb354d4d8e2af1be95
Kim hyunseok <dorrdorr9311@gmail.com>
2022-03-23 11:51:06 +0900

    boj-2800 괄호제거 문제해결

--- recursion/boj-2800.js
+++ recursion/boj-2800.js
@@ -1,31 +1,49 @@
-const expression = require('fs').readFileSync('example.txt').toString().trim().split('');
+const expression = require('fs').readFileSync('dev/stdin').toString().trim().split('');
 
 function solution(expression) {
-  console.log(recursion(expression));
+  const bracketPositions = [];
+  const stack = [];
+  const result = new Set();
+
+  for (let i = 0; i < expression.length; i++) {
+    if (expression[i] === ')') {
+      while (expression[stack[stack.length - 1]] !== '(') {
+        stack.pop();
+      }
+      const openIndex = stack.pop();
+      const closeIndex = i;
+      bracketPositions.push([openIndex, closeIndex]);
+    }
+    stack.push(i);
+  }
+
+  for (let i = 1; i <= bracketPositions.length; i++) {
+    getCombination(bracketPositions, i).forEach((removePositions) => {
+      const bracketRemovedExpression = expression
+        .filter((_, index) => !removePositions.flat().includes(index))
+        .join('')
+        .trim();
+      result.add(bracketRemovedExpression);
+    });
+  }
+
+  const sorted = [...result].sort();
+  console.log(sorted.join('\n'));
 }
 
-function recursion(subExpression) {
+function getCombination(array, number) {
+  if (number === 1) return array.map((el) => [el]);
   const result = [];
-  let prefix = '';
-  //  console.log(subExpression);
 
-  for (let i = 0; i < subExpression.length; i++) {
-    const char = subExpression[i];
+  for (let i = 0; i < array.length; i++) {
+    const fixed = array[i];
+    const rest = array.slice(i + 1);
 
-    if (char === '(') {
-      const nextSubExpression = subExpression.slice(i + 1);
-      recursion(nextSubExpression).forEach(([el, index]) => {
-        result.push(prefix + el);
-        result.push(prefix + '(' + el + ')');
-        i = index + 1;
-      });
-    } else if (char === ')') {
-      result.push([prefix, i]);
-      return result;
-    } else {
-      prefix += char;
-    }
+    getCombination(rest, number - 1).forEach((comb) => {
+      result.push([fixed, ...comb]);
+    });
   }
+  return result;
 }
 
 solution(expression);
